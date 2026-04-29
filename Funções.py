4f121d8b83d6480683175c1d8cc788d230b264e4
Antonio Abreu <antonio62479146@aluno.ms.senac.br>
2026-04-28 20:00:53 -0400

    first commit

---
+++ Funções.py
@@ -0,0 +1,147 @@
+import json
+def Ler_archive(arquivo):
+    with open(arquivo, "r") as archive:
+        resultado = json.load(archive)
+    return resultado
+
+def Adicionar_archive(dicionario, arquivo):
+    with open(arquivo, "w") as archive:
+        json.dump(dicionario, archive, indent=2)
+
+#Menu usuarios.
+def menu_users():
+    print("1- Adminstrador")
+    print("2- Recepcionista")
+    print("3- Médico")
+
+def validar_admin(lista):
+     login = False
+     user = input("Digite seu usuario: ")
+     password = input("DIgite sua senha: ")
+     for p in lista:
+            if user == p["user"] and password == p["password"]:
+                print(f"Bem vindo Sr {p["name"]}")
+                login = True
+            else:
+                print("Senha Incorreta! ")
+            return login
+def cadastrar_user(lista):
+    name = input("Digite o nome do Usuario: ")
+    password = input("Cadastre a senha: ")
+    nivel = input("Digite o nível da área: ")
+    dados = {"id": id_users,
+            "user": name,
+            "password": password,
+            "nivel": nivel}
+    lista.append(dados)
+    id_users(lista)
+    Adicionar_archive(lista, "users.json")
+def op_cadastrar_user():
+    print("--------------O que deseja alterar? --------------")
+    print("1-Cadastrar usuários do sistema  ")
+    print("2-Editar usuários    ")
+    print("3-Excluir usuários   ")
+    print("4-Resetar senha de usuários    ")
+    print("5-Listar todos os usuários    ")
+    print("--------------------------------------------------")
+def Remove_user(lista):
+    for p in lista:
+        print(f"Id: {p["id"]}: Usuario: {p["user"]} Nível: {p["nivel"]}")
+    op = int(input("Escolha o id do usuario que deseja excluir "))
+    for i in lista:
+        if op == i["id"]:
+            lista.pop(op-1)
+            id_users(lista)
+            Adicionar_archive(lista, "users.json")
+def options():
+    print("--------------------------------------------------")
+    print("1- Usuarios")
+    print("2- médicos")
+    print("3- pacientes")
+    print("4- Consultas")
+    print("5- Relatorios")
+    print("--------------------------------------------------")
+def reset_password(lista):
+    for i in lista:
+        print(f"Id: {i["id"]}: Usuario: {i["user"]} Nível: {i["nivel"]}")
+    op = int(input("Digite o Id do usuario que quer alterar a senha: "))
+    for i in lista:
+        if op == i["id"]:
+            print(f"Usuario {i["user"]} escolhido.")
+            password = input("Digite a nova senha : ")
+            i["password"] = password
+                                
+            Adicionar_archive(lista, "users.json")
+def view_users(lista):
+    for i in lista:
+        print(f"Nome: {i["user"]} Nível: {i["nivel"]}")
+        
+
+
+#cadastrar médicos: admin
+def medicos_cdt():
+    print("1-Cadastrar médicos   ")
+    print("2-Editar usuários    ")
+    print("3-Editar médicos     ")
+    print("4-Excluir médicos      ")
+    print("5-Listar médicos cadastrados   ")
+    
+
+
+
+
+
+#cadastrar paciente: admin
+def pacientes_cdt():
+    print("1-Visualizar todos os pacientes     ")
+    print("2-Buscar pacientes      ")
+    print("3-Editar médicos     ")
+    print("4-Excluir médicos      ")
+    print("5-Ver histórico completo     ")
+def mudanças_admin():
+    print("--------------Escolha o que deseja alterar--------------")
+    print("1- usuarios")
+    print("2- medicos")
+    print("3- Pacientes")
+    print("-------------- Outras opções--------------")
+    print("4- Consultas")
+    print("5- Relatórios")
+
+
+    
+
+
+#Recepcionista.
+def perm_recepcionista():
+    print("1- Cadastrar paciente")
+    print("2- Editar paciente")
+    print("3- Buscar paciente específico")
+    print("4- Listar todos os pacientes")
+    print("5- Visualizar dados completos")
+    print("6- Consultas")
+    
+    
+def consultas():
+    print("1- Marcar consultas para médico") #Escolhar data e horario dentro
+    print("Escolher data e horário")
+    print("Reagendar consulta")
+    print("Cancelar consulta")
+    print("Confirmar presença do paciente")
+    print("Listar todas as consultas do dia")
+    print("Listar consultas")
+    
+
+def Historico():
+    print("1- Visualizar consultas anteriores do paciente")
+    print("2- Ver datas de atendimentos anteriores")
+def Relatórios():
+    print("Agenda do dia")
+    print("Consultas por data")
+    print("Consultas canceladas no período")
+    print("pacientes atendidos no dia")
+    
+def id_users(lista):
+    id = 0
+    for i in lista:
+        id +=1
+        i["id"] = id
---
+++ Main.py
@@ -0,0 +1,28 @@
+import json
+import Funções as f
+view = f.Ler_archive("users.json")
+view_pacientes = f.Ler_archive("pacientes.json")
+view_medicos= f.Ler_archive("consultas.json")
+view_prontuarios = f.Ler_archive("prontuarios.json")
+view_consultas = f.Ler_archive("consultas.json")
+while True:
+    f.menu_users()
+    op = input("Digite sua opção")
+    if op == "1":
+        if f.validar_admin(view) == True:
+            while True:
+                f.options()
+                op = input("Digite o que deseja alterar")
+                if op == "1":
+                    f.op_cadastrar_user()
+                    op1 = input("DIgite sua opção")
+                    if op1 == "1":
+                     f.cadastrar_user(view)
+                
+                    elif op1 == "3":
+                        f.Remove_user(view)
+                    elif op1 == "4":
+                        f.reset_password(view)
+                    elif op1 == "5":
+                        for i in view:
+                            f.view_users(view)
